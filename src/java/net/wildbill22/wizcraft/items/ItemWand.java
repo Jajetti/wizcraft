534e98c526b5139d32779eb6f9e8739acd0b471b
Jajetti <bubblegumwarriors669@gmail.com>
2015-04-12 13:41:12 +0100

    Create ItemWand.java

---
+++ src/java/net/wildbill22/wizcraft/items/ItemWand.java
@@ -0,0 +1,15 @@
+package net.willbill22.items;
+
+import net.minecraft.item.Item;
+
+
+
+
+public class ItemWand extends Item {
+        private String textureName;
+        
+public ItemWand() {
+	    super(material);
+	    this.setUnlocalizedName("wand");
+	    this.setTextureName(Reference.MODID + ":" + "wand";
+	}
